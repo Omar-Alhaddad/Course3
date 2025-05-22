1406223d754c9ba13f47385a9db414bb458bda99
Omar-Alhaddad <omar.alhaddady@gmail.com>
2025-05-22 14:42:23 +0300

    Add Lesson12Homework1

---
+++ Lesson12Homework1/Lesson12Homework1/Lesson12Homework1.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main()
+{
+    //My Information Card
+    std::cout << "******************************************\n";
+    std::cout << "Name: Omar Al-haddad\n";
+    std::cout << "Age : 44 Years\n";
+    std::cout << "City: Mukalla\n";
+    std::cout << "Country: " << "Yemen\n";
+    std::cout << "******************************************\n";
+
+    return 0;
+}
