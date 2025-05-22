3415dc92ac8982e04cb4515a53026c9e0f3dfacc
Omar-Alhaddad <omar.alhaddady@gmail.com>
2025-05-22 14:59:50 +0300

    Add Lesson12Homework2

---
+++ Lesson12Homework2/Lesson12Homework2/Lesson12Homework2.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+int main()
+{
+    //Squar Of Stars
+    std::cout << "********\n";
+    std::cout << "********\n";
+    std::cout << "********\n";
+    std::cout << "********\n";
+
+    return 0;
+}
