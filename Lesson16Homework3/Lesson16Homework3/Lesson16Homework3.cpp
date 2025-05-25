6f37f715a9f96efe2b26dde16a4c7e890d3f6c9c
Omar-Alhaddad <omar.alhaddady@gmail.com>
2025-05-26 00:45:07 +0300

    Add Lesson16Homework3

---
+++ Lesson16Homework3/Lesson16Homework3/Lesson16Homework3.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Table of Names in 3 Columns
+    cout << "Ali\tAhmed\tLina \n";
+    cout << "Fadi\tZain\tMona \n";
+
+    return 0;
+}
