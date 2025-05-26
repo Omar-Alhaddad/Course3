464191eeaebfed8e245952f8f75e0894b381dde4
Omar-Alhaddad <omar.alhaddady@gmail.com>
2025-05-26 19:30:16 +0300

    Add Lesson17Homework1

---
+++ Lesson17Homework1/Lesson17Homework1/Lesson17Homework1.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //My Information Card
+    string Name = "Omar Al-haddad";
+    int Age = 27;
+    string City = "Mukalla";
+    string Country = "Yemen";
+    float MonthlySalary = 5000;
+    float YearlySalary = MonthlySalary * 12;
+    char Gender = 'M';
+    bool isMarried = false;
+
+    cout << "*****************************************\n";
+    cout << "Name: " << Name << endl;
+    cout << "Age : " << Age << " Years" << endl;
+    cout << "City : " << City << endl;
+    cout << "Country : " << Country << endl;
+    cout << "Monthly Salary : " << MonthlySalary << endl;
+    cout << "Yearly Salary : " << YearlySalary << endl;
+    cout << "Gender : " << Gender << endl;
+    cout << "Married : " << isMarried << endl;
+    cout << "*****************************************\n";
+
+    return 0;
+}
