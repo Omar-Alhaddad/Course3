c676cc3eb94fde8711abdb969ebb385753dc3479
Omar-Alhaddad <omar.alhaddady@gmail.com>
2025-05-26 19:40:33 +0300

    Add Lesson17Homework3

---
+++ Lesson17Homework3/Lesson17Homework3/Lesson17Homework3.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // User's Age After 5 Years
+    int Age = 25;
+    int YourAgeAfter5Years = Age + 5;
+
+    cout << "After 5 years you will be " << Age + 5 << " years old." << endl;
+    cout << "After 5 years you will be " << YourAgeAfter5Years << " years old." << endl;
+
+    return 0;
+}
