c303c08b31c18256c48a9e2489d0ef1687a3f248
Omar-Alhaddad <omar.alhaddady@gmail.com>
2025-05-26 19:36:22 +0300

    Add Lesson17Homework2

---
+++ Lesson17Homework2/Lesson17Homework2/Lesson17Homework2.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Sum of Three Numbers
+    int a = 20, b = 30, c = 10;
+
+    int Sum = a + b + c;
+
+    cout << a << " + " << endl;
+    cout << b << " + " << endl;
+    cout << c << endl;
+    cout << "--------------" << endl;
+
+    cout << Sum << endl;
+
+    return 0;
+}
