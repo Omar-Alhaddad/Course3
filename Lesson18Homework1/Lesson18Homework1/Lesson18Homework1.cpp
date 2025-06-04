3ef532901fac6b821fe4b74e9cb48a8d69f95fc2
Omar-Alhaddad <omar.alhaddady@gmail.com>
2025-06-04 06:23:34 +0300

    Add Lesson18Homework1

---
+++ Lesson18Homework1/Lesson18Homework1/Lesson18Homework1.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //My Information Card
+    string Name;
+    int Age;
+    string City;
+    string Country;
+    float MonthlySalary;
+    char Gender;
+    bool isMarried;
+
+    cout << "Please enter Your Name?" << endl;
+    cin >> Name;
+
+    cout << "Please enter Your Age?" << endl;
+    cin >> Age;
+
+    cout << "Please enter Your City?" << endl;
+    cin >> City;
+
+    cout << "Please enter Your Country?" << endl;
+    cin >> Country;
+
+    cout << "Please enter Your Monthly Salary?" << endl;
+    cin >> MonthlySalary;
+
+    cout << "Please enter Your Gender M/F ?" << endl;
+    cin >> Gender;
+
+    cout << "Are you married 1/0 ?" << endl;
+    cin >> isMarried;
+
+    cout << "*****************************************\n";
+    cout << "Name: " << Name << endl;
+    cout << "Age : " << Age << endl;
+    cout << "City : " << City << endl;
+    cout << "Country : " << Country << endl;
+    cout << "Monthly Salary : " << MonthlySalary << endl;
+    cout << "Yearly Salary : " << MonthlySalary * 12 << endl;
+    cout << "Gender : " << Gender << endl;
+    cout << "Married : " << isMarried << endl;
+    cout << "*****************************************\n";
+
+    return 0;
+}
