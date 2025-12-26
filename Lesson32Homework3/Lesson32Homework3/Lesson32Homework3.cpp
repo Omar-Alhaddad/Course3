790ffe256b8f6a0bd73f4222f5ec45703181c34e
Omar-Alhaddad <omar.alhaddady@gmail.com>
2025-12-26 16:53:02 +0300

    Add Lesson32Homework3

---
+++ Lesson32Homework3/Lesson32Homework3/Lesson32Homework3.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+void printMyPassion() {
+
+	//My Passion for Programming 
+	cout << "I Love Programming!\n\n"
+		 << "I promise to be the best developer ever!\n\n"
+		 << "I know it will take some time to practice, but I will achieve my goal.\n\n"
+		 << "Best Regards,\n"
+		 << "Omar Al-haddad.\n";
+}
+
+int main()
+{
+	printMyPassion();
+
+	return 0;
+}
